a9beca3096b7f53a8fade3dd7a23435f3d167407
roland <rolandrocking@gmail.com>
2023-05-15 15:53:34 +0400

    Modified permmission overview page but still work to do

--- lib/permission_overview_page.dart
+++ lib/permission_overview_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'package:device_apps/device_apps.dart';
+import 'package:permission_handler/permission_handler.dart';
+
 
 class PermissionsOverview extends StatefulWidget {
   @override
@@ -8,12 +11,16 @@ class PermissionsOverview extends StatefulWidget {
 
 class _PermissionsOverviewState extends State<PermissionsOverview> {
   Map<Permission, PermissionStatus> _permissions = {};
+  Map<Permission, List<Application>> _permissionApps = {};
 
-  @override
   void initState() {
     super.initState();
     _fetchPermissions();
+    Permission.values.forEach((permission) {
+      _fetchPermissionApps(permission);
+    });
   }
+
 
   void _fetchPermissions() async {
     final permissions = await Permission.values;
@@ -23,33 +30,148 @@ class _PermissionsOverviewState extends State<PermissionsOverview> {
     });
   }
 
-  Widget _buildPermissionTile(Permission permission) {
-    final status = _permissions[permission];
-    return ListTile(
-      title: Text(permission.toString()),
-      subtitle: Text(status.toString()),
-      trailing: ElevatedButton(
-        onPressed: () {
-          permission.request().then((status) {
-            setState(() {
-              _permissions[permission] = status;
-            });
-          });
-        },
-        child: Text("Change"),
-      ),
-    );
+  void _fetchPermissionApps(Permission permission) async {
+  final apps = await DeviceApps.getInstalledApplications(
+    includeAppIcons: true,
+    includeSystemApps: true,
+  );
+
+    setState(() {
+      _permissionApps[permission] = apps
+          .where((app) =>
+              app.packageName.isNotEmpty &&
+              _hasPermission(app.packageName!, permission))
+          .toList();
+    });
   }
+
+
+  bool _hasPermission(String packageName, Permission permission) {
+    if (_permissionApps.containsKey(permission)) {
+      final apps = _permissionApps[permission];
+      return apps!.any((app) => app.packageName == packageName);
+    }
+    return false;
+  }
+
+
+  void _requestPermission(Permission permission) async {
+    final status = await permission.request();
+    if (status == PermissionStatus.granted) {
+      _fetchPermissionApps(permission);
+    }
+  }
+
+
+
+  void _openAppSettings() {
+    openAppSettings();
+  }
+
+  Widget _buildPermissionTile(Permission permission) {
+  final status = _permissions[permission];
+  final apps = _permissionApps[permission];
+
+  IconData icon;
+  Color iconColor;
+
+  if (permission.toString().contains("location")) {
+    icon = Icons.location_on;
+    iconColor = Colors.blue;
+  } else if (permission.toString().contains("camera")) {
+    icon = Icons.camera_alt;
+    iconColor = Colors.orange;
+  } else if (permission.toString().contains("contacts")) {
+    icon = Icons.contacts;
+    iconColor = Colors.green;
+  } else if (permission.toString().contains("microphone")) {
+    icon = Icons.mic;
+    iconColor = Colors.red;
+  } else if (permission.toString().contains("storage")) {
+    icon = Icons.sd_card;
+    iconColor = Colors.deepPurple;
+  } else if (permission.toString().contains("calendar")) {
+    icon = Icons.calendar_today;
+    iconColor = Colors.teal;
+  } else if (permission.toString().contains("phone")) {
+    icon = Icons.phone;
+    iconColor = Colors.indigo;
+  } else if (permission.toString().contains("sms")) {
+    icon = Icons.sms;
+    iconColor = Colors.amber;
+  } else {
+    icon = Icons.info;
+    iconColor = Colors.grey;
+  }
+
+
+  return ExpansionTile(
+    leading: Icon(icon, color: iconColor),
+    title: Text(permission.toString().substring(permission.toString().indexOf('.') + 1)),
+    subtitle: Text(status.toString().substring(status.toString().indexOf('.') + 1)),
+    trailing: Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        ElevatedButton(
+          onPressed: () => _requestPermission(permission),
+          child: Text("Grant"),
+        ),
+        SizedBox(width: 8),
+        ElevatedButton(
+          onPressed: () => _openAppSettings(),
+          child: Text("Revoke"),
+        ),
+      ],
+    ),
+    children: apps != null
+        ? apps.map((app) => ListTile(
+            leading: Icon(Icons.info), // Use a placeholder icon
+            title: Text(app.appName ?? ""),
+            subtitle: Text(app.packageName ?? ""),
+          )).toList()
+        : [],
+  );
+}
+
+
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Permissions Overview"),
-      ),
-      body: ListView(
-        children: Permission.values.map((permission) => _buildPermissionTile(permission)).toList(),
-      ),
-    );
+  return Scaffold(
+  appBar: AppBar(
+  title: Text("Permissions Overview"),
+  ),
+  body: ListView(
+  children: Permission.values
+  .map((permission) => _buildPermissionTile(permission))
+  .toList(),
+  ),
+  );
   }
 }
+
+// class AppListPage extends StatelessWidget {
+//   final List<Application>? apps;
+
+//   const AppListPage({Key? key, this.apps}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text("App List"),
+//       ),
+//       body: ListView.builder(
+//         itemCount: apps?.length ?? 0,
+//         itemBuilder: (context, index) {
+//           final app = apps![index];
+//           return ListTile(
+//             leading: app.icon != null ? Image.memory(app.icon!) : SizedBox(),
+//             title: Text(app.appName ?? ""),
+//             subtitle: Text(app.packageName ?? ""),
+//           );
+//         },
+//       ),
+//     );
+//   }
+// }
